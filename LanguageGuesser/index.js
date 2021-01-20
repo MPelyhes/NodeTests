54649349860cee3b6e287f5bf2e8a64a9905e53c
Matt Pelyhes <mattpelyhes@gmail.com>
2021-01-20 12:27:13 -0600

    Add aditional functionality to language guesser so that it accepts an argument from the terminal.  Added colors package so that errors show up in red and success shows up green.

--- LanguageGuesser/index.js
+++ LanguageGuesser/index.js
@@ -1,5 +1,11 @@
 const franc = require('franc');
 const langs = require('langs');
-
-const francResult = (franc('এটি একটি ভাষা একক IBM স্ক্রিপ্ট'));
-console.log(langs.where('3', francResult).name);
+const colors = require('colors')
+const input = process.argv[2];
+const langCode = franc(input);
+if(langCode === 'und'){
+    console.log('Sorry, could not figure it out! Try again with more sample text.'.red)
+} else {
+  const language = (langs.where('3', langCode).name);
+  console.log(`Our best guess is: ${language}`.green)
+}
