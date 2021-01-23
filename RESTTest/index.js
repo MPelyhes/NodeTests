fec337587b3065dfb850080f9db668e101692f6e
Matt Pelyhes <mattpelyhes@gmail.com>
2021-01-23 12:32:21 -0600

    Add show posts on our inde.ejs page using a get request

--- RESTTest/index.js
+++ RESTTest/index.js
@@ -1,2 +1,34 @@
+const path = require('path');
+const methodOverride = require('method-override');
+const {v4: uuid} = require('uuid'); // For generating unique ID's
 const express = require('express');
 const app = express();
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+let userPosts = [
+  {
+    id: uuid(),
+    bookTitle: 'Moby Dick',
+    bookSum:  'Ishmael goes on a whaling vessel captained by Ahab. Ahab hates a white whale named Moby, and makes it his life goal to kill the great white whale. There is a lot of symbolism in this book'
+  },
+  {
+    id: uuid(),
+    bookTitle: 'Slaughterhouse Five',
+    bookSum: 'Dark comedy and WWII flashbacks abound in this classic.'
+  },
+]
+//**************************************************
+// INDEX - Renders multiple posts
+//**************************************************
+
+app.get('/posts', (req, res) => {
+  res.render('posts/index', { userPosts});
+})
+
+
+app.listen(3000, () => {
+  console.log("ON PORT 3000!")
+})
