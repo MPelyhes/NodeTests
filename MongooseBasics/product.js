72a363dd40b0a848fd9317de98745027e78286b1
Matt Pelyhes <mattpelyhes@gmail.com>
2021-01-26 16:55:01 -0600

    Add schema constraints

--- MongooseBasics/product.js
+++ MongooseBasics/product.js
@@ -11,17 +11,34 @@ mongoose.connect('mongodb://localhost:27017/shopApp', {useNewUrlParser: true, us
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+    maxlength: 20
   },
   price: {
     type: Number,
-    required: true 
+    required: true,
+    min: 0
+  },
+  onSale: {
+    type: Boolean,
+    default: false
+  },
+  categories: [String],
+  qty: {
+    online: {
+      type: Number,
+      default: 0
+    },
+    inStore: {
+      type: Number,
+      default:0
+    }
   }
 });
 
 const Product = mongoose.model('Product', productSchema)
 
-const bike = new Product({name: 'Mountain Bike', price: 599})
+const bike = new Product({name: 'Racing Bike', price: 2999.50, categories: ['Cycling', 'Safety']})
 bike.save()
   .then(data => {
     console.log('It worked!')
