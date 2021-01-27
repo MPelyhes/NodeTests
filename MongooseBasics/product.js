d9687c3684ea61e137cf9da6f7785216d3c96e51
Matt Pelyhes <mattpelyhes@gmail.com>
2021-01-26 21:07:23 -0600

    Add a virtual to the person.js file

--- MongooseBasics/product.js
+++ MongooseBasics/product.js
@@ -36,15 +36,25 @@ const productSchema = new mongoose.Schema({
   }
 });
 
+productSchema.methods.greet = function () {
+  console.log('Hey, Hi, How yur durin?')
+}
+
+productSchema.statics.fireSale = function() {
+ return this.updateMany({}, {onSale: true, price: 0})
+}
+
 const Product = mongoose.model('Product', productSchema)
 
-const bike = new Product({name: 'Racing Bike', price: 2999.50, categories: ['Cycling', 'Safety']})
-bike.save()
-  .then(data => {
-    console.log('It worked!')
-    console.log(data)
-  })
-  .catch(err => {
-    console.log('Oh no! Error!')
-    console.log(err)
-  })
+// const bike = new Product({name: 'Racing Bike', price: 2999.50, categories: ['Cycling', 'Safety']})
+// bike.save()
+//   .then(data => {
+//     console.log('It worked!')
+//     console.log(data)
+//   })
+//   .catch(err => {
+//     console.log('Oh no! Error!')
+//     console.log(err)
+//   })
+
+  Product.fireSale().then(res => console.log(res))
