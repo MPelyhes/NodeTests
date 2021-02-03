d0f0b6e634ec1bffc6d9aa5b6b3fc3287232a44d
Matt Pelyhes <mattpelyhes@gmail.com>
2021-02-03 17:58:06 -0600

    add signed cookies

--- CookiesDem/index.js
+++ CookiesDem/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+app.use(cookieParser('thisismysecret'));
 
 app.get('/greet', (req, res) => {
   const {name = 'No-name'} = req.cookies;
@@ -12,6 +12,17 @@ app.get('/greet', (req, res) => {
 app.get('/setname', (req, res) => {
   res.cookie('name', 'Stevie Chicks')
   res.send('Ok sent you a cookie!')
+})
+
+app.get('/getsignedcookie', (req, res) => {
+  res.cookie('fruit', 'grape', { signed: true});
+  res.send('Okay! Signed your fruit cookie!')
+})
+
+app.get('/verifyfruit', (req, res) => {
+  console.log(req.cookies);
+  console.log(req.signedCookies);
+  res.send(req.signedCookies);
 })
 
 app.listen(3001, () => {
