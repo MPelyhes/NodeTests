04d2e5988408e3f164261ec27457f4caf5b6126f
Matt Pelyhes <mattpelyhes@gmail.com>
2021-02-05 17:49:20 -0600

    add bcrypt demo

---
+++ BcryptDemo/index.js
@@ -0,0 +1,25 @@
+const bcrypt = require('bcrypt');
+
+// const hashPassword = async (pw) => {
+//   const salt = await bcrypt.genSalt(12);
+//   const hash = await bcrypt.hash(pw, salt)
+//   console.log(salt);
+//   console.log(hash);
+// }
+
+const hashPassword = async (pw) => {
+  const hash = await bcrypt.hash(pw, 12);
+  console.log(hash);
+}
+
+const login = async (pw, hashedPw) => {
+  const result = await bcrypt.compare(pw, hashedPw)
+  if(result){
+    console.log('Logged you in!')
+  } else {
+    console.log('Incorrect!')
+  }
+}
+// hashPassword('monkey');
+
+login('monkey', '$2b$12$1auEb8q22avHI.ukGYhTxuWhi/pio5aipDDJnRtQ9RJ/XhHqwhw/6');
